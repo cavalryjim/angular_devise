1e38de6684f76b5c07eda74c2d017cdc502ef8fd
James Davis <james.davisphd@gmail.com>
2014-10-24 17:49:25 -0500

    initial push

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AngularDevise::Application.config.secret_key_base = '5153a26e8839c8131f366fc7c565a7b0acc710ec31e2237de7b2181c3030edcaac4819f93d64ebff89cd14727ad21f76ef3813c813b774c8865c6f01203f48cf'
